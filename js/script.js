135380a39efba6945736d84f0a4337145e18434a
Amandar8 <amandaruth08@gmail.com>
2018-09-04 15:47:42 -0500

    converted jQuery to javascript

--- js/script.js
+++ js/script.js
@@ -1,54 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-  // function to create cirle of icons
-  let type = 1, //circle type - 1 whole, 0.5 half, 0.25 quarter
-    radius = '12em', //distance from center
-    start = -90, //shift start from 0
-    elements = $('li:not(:first-child)'), //puts first li element in center of circle
-    numberOfElements = (type === 1) ? elements.length : elements.length - 1, //adjusts for even distribution of elements when not full circle
-    slice = 360 * type / numberOfElements;
+  const elements = document.querySelectorAll('li:not(:first-child)');
+  const slice = 360 / elements.length;
 
-  elements.each(function(i) {
-    let self = $(this),
-      rotate = slice * i + start,
-      rotateReverse = rotate * -1;
-
-    self.css({
-      'transform': 'rotate(' + rotate + 'deg) translate(' + radius + ') rotate(' + rotateReverse + 'deg)'
-    });
+  Array.from(elements).forEach(function (element, i) {
+    const rotate = slice * i - 90;
+    const rotateReverse = rotate * -1;
+    element.style.transform = 'rotate(' + rotate + 'deg) translate( 13em ) rotate(' + rotateReverse + 'deg)';
   });
-
-  let astroSign = document.getElementsByClassName('signIcon')
+  
+  let astroSign = document.getElementsByClassName('signIcon');
 
   // this creates an array of elements instead of an html collection
-  Array.from(astroSign).forEach(function(astroSignButton) {
+  Array.from(astroSign).forEach(function (astroSignButton) {
     // iterating over each button in the array and adding an event listener
     astroSignButton.addEventListener('click', astroSignButtonPressed)
   });
 
-// document.getElementById('popUp').addEventListener('mouseenter', function(event){
-//   event.target.addEventListener('mouseleave', function(event){
-//       event.target.classList.add
-//   });
-// })
-
-// create mouseover and mouseout effect when scrolling over icon
+  // create mouseover and mouseout effect when scrolling over icon
   for (let i = 0; i < astroSign.length; i++) {
-      astroSign[i].addEventListener('mouseover', mouseOverEffect);
-      astroSign[i].addEventListener('mouseout', mouseOutEffect);
+    astroSign[i].addEventListener('mouseover', mouseOverEffect);
+    astroSign[i].addEventListener('mouseout', mouseOutEffect);
   }
 
-// mouseover effect that changes astro name font
+  // mouseover effect that changes astro name font
   function mouseOverEffect() {
     this.classList.add("astroSign-highlight");
   }
 
-// removes astro name font
+  // removes astro name font
   function mouseOutEffect() {
     this.classList.remove("astroSign-highlight");
   }
 
-// function that creates modals when each zodiac icon is clicked
+  // function that creates modals when each zodiac icon is clicked
   function astroSignButtonPressed(event) {
     let zodiacSign = this.id;
     console.log('Zodiac Sign:', zodiacSign);
@@ -56,8 +41,6 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   let title = document.getElementById('title');
   let para = document.getElementById('para');
-  title.innerHTML = "Loading...";
-  para.innerHTML = "";
 
   // function to obtain API horoscope information
   function fetchHoroscope(zodiacSign) {
@@ -69,14 +52,12 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-      console.log('Horoscope Data:', data)
+        console.log('Horoscope Data:', data)
 
-      let horoName = document.getElementById(zodiacSign);
+        title.innerHTML = zodiacSign;
+        para.innerHTML = data.horoscope;
 
-      title.innerHTML = zodiacSign;
-      para.innerHTML = data.horoscope;
-
-    })
+      })
 
       .catch(error => console.log(error));
 
